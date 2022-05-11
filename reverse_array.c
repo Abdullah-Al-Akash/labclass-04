b3af3b881391b8005ae50a8f6bfbb7da3643aa95
Abdullah Al Akash <aabdullahalakash@gmail.com>
2022-05-11 16:29:32 +0600

    Reverse Array Complete

---
+++ reverse_array.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    int array[n];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d", &array[i]);
+    }
+    //Make Reverse:
+    /*int array2[n];
+    for(int i=0; i<n; i++)
+    {
+        array2[n-1-i] = array[i];
+    }*/
+    for(int i=0, j=n-1; i<j; i++, j--)
+    {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    //Print reverse Array
+    for(int i=0; i<n; i++)
+    {
+        printf("%d ",array[i]);
+    }
+
+
+    return 0;
+}
